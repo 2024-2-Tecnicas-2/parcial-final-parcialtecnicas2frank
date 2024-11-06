d8dfe5dcb4ca8852603acb8350ff91e175e184e5
... <scamargomontoya@gmail.com>
2024-11-06 18:44:08 -0500

    Clase perro
    Se extiende e implementa sin agregar metodos

--- src/main/java/logicaNegocio/Animal.java
+++ src/main/java/logicaNegocio/Animal.java
@@ -4,4 +4,5 @@ public abstract class Animal {
     
     public abstract String emitirSonido();
     public abstract String obtenerDieta();
+    public abstract TipoHabitat getHabitat();
 }
--- src/main/java/logicaNegocio/Perro.java
+++ src/main/java/logicaNegocio/Perro.java
@@ -1,5 +1,15 @@
 package logicaNegocio;
 
-public class Perro {
+public class Perro extends Animal implements Domestico{
+
+    @Override
+    public String emitirSonido() {
+        return null;
+    }
+
+    @Override
+    public String obtenerDieta() {
+        return null;
+    }
     // TODO: Aquí va tu código
 }
