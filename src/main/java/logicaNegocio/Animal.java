7d3041460bee449c5170387c900045a0c836040f
... <scamargomontoya@gmail.com>
2024-11-06 18:22:07 -0500

    Se crean los metodos de la clase abstract

--- src/main/java/logicaNegocio/Animal.java
+++ src/main/java/logicaNegocio/Animal.java
@@ -1,5 +1,7 @@
 package logicaNegocio;
 
 public abstract class Animal {
-    // TODO: Aquí va tu código
+    
+    public abstract String emitirSonido();
+    public abstract String obtenerDieta();
 }
