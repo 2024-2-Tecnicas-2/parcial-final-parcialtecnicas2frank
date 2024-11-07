3cf4e3c95e602809cc87d23bba6cb90ca3b43df7
pepito <pepitoperez@gmail.com>
2024-11-06 19:04:35 -0500

    Se crean objetos main Animales

--- src/main/java/logicaNegocio/Animales.java
+++ src/main/java/logicaNegocio/Animales.java
@@ -3,9 +3,21 @@ package logicaNegocio;
 public class Animales {
     
     public static void main(String []args){
-        Animal perro = new Perro();
+        Perro perro = new Perro();
         Animal leon = new Leon();
         Animal delfin = new Delfin();
-
+        
+        System.out.println("Perro");
+        perro.emitirSonido();
+        perro.obtenerDieta();
+        perro.interactuarConHumano();
+        System.out.println("");
+        System.out.println("Leon");
+        leon.emitirSonido();
+        leon.obtenerDieta();
+        System.out.println("");
+        System.out.println("Delfin");
+        delfin.emitirSonido();
+        delfin.obtenerDieta();
     }
 }
